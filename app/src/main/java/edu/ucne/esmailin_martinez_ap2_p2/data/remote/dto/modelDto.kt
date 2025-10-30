6ca48130cf6774b4d7467ebb7af26b031f2c4246
ESMAILINM <esmailinmartinez05@gmail.com>
2025-10-29 21:15:32 -0400

    agregar carpetas

---
+++ app/src/main/java/edu/ucne/esmailin_martinez_ap2_p2/data/remote/dto/modelDto.kt
@@ -0,0 +1,5 @@
+package edu.ucne.esmailin_martinez_ap2_p2.data.remote.dto
+
+//data
+class modelDto {
+}
---
+++ app/src/main/java/edu/ucne/esmailin_martinez_ap2_p2/presentacion/edit/screen.kt
@@ -0,0 +1,4 @@
+package edu.ucne.esmailin_martinez_ap2_p2.presentacion.edit
+
+class screen {
+}
